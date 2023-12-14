b13c9de1d9e6d56cfaf735f07e1329677e41b864
Chang Su <chang.s.su@oracle.com>
2023-12-14 22:43:29 +0000

    Pull request #3: Revert logging to non-struct logging
    Merge in GEN/vllm from chang/log to feature-based-on-v0.2.3.0
    
    Squashed commit of the following:
    
    commit 5ed64dae27a4bc7df62b1755bfc52c38608a0792
    Author: Chang Su <chang.s.su@oracle.com>
    Date:   Thu Dec 14 14:18:16 2023 -0800
    
        Revert logging to non-struct logging
    
        - Revert struct log to non-struct log as JSON format is not shown by Lumberjack
        - Remove structlog in requirements.txt
        - Make opc-request-id only processed when request is POST method

--- vllm/entrypoints/openai/api_server.py
+++ vllm/entrypoints/openai/api_server.py
@@ -20,8 +20,7 @@ from fastapi.responses import JSONResponse, StreamingResponse, Response
 from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm.engine.metrics import add_global_metrics_labels
-from vllm.entrypoints.openai.middleware import (
-    ExtendedMetricsMiddleware, OpcRequestIdLoggingMiddleware)
+from vllm.entrypoints.openai.middleware import ExtendedMetricsMiddleware
 from vllm.entrypoints.openai.protocol import (
     CompletionRequest, CompletionResponse, CompletionResponseChoice,
     CompletionResponseStreamChoice, CompletionStreamResponse,
@@ -88,7 +87,27 @@ def parse_args():
 
 app.add_middleware(ExtendedMetricsMiddleware)  # Trace HTTP server metrics
 app.add_route("/metrics", metrics)  # Exposes HTTP metrics
-app.middleware("http")(OpcRequestIdLoggingMiddleware(__name__))  # Extended Logging middleware
+
+
+@app.middleware("http")
+async def log_opc_header(request: Request, call_next):
+    # Log at the start and end of a POST request
+    if request.method == "POST":
+        opc_request_id = request.headers.get("opc-request-id", "unknown")
+        logger.info(f"POST Request Start - opc-request-id: {opc_request_id}")
+
+        try:
+            response = await call_next(request)
+            logger.info(f"POST Request End - opc-request-id: {opc_request_id}, "
+                        f"status_code: {response.status_code}")
+            return response
+        except Exception as e:
+            logger.error(f"Exception during POST request with "
+                         f"opc-request-id: {opc_request_id}, error: {e}")
+            raise
+
+    # For non-POST requests, just pass through
+    return await call_next(request)
 
 
 def create_error_response(status_code: HTTPStatus,
