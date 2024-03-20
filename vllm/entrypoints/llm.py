630f397ae37232cd8d4c8a2e4153f8674282fb53
Chang Su <chang.s.su@oracle.com>
2024-03-19 18:47:52 -0700

    Add Embedding generation
    Add Embedding API to entrypoints/openai
    
    - Add EmbeddingRequest and Response to protocol
    - Add serving_embedding
    - Add OpenAIServingEmbedding to api_server
    - Make llm.py work with embedding
    
    Add Embedding in outputs and sequence
    
    - Add EmbeddingOutput, EmbeddingRequestOutput, RequestOutputFactory and
    EmbeddingSequenceGroupOutput to support process the embedding output
    sequence
    - Update process output and sequence in *llm_engine to use embedding
    
    Add MistralModel and embedding generation
    
    - Add embedding and load_weights in LlamaModel to support forward
    - Adapted from code examples in https://huggingface.co/intfloat/e5-mistral-7b-instruct
    - Ensures the ops are run on GPU.
    - Mistral uses LlamaModel
    - Use embedding when embedding_mode is True in model_runner
    
    Uniform typing in multiple files
    
    - Add EmbeddingRequestOutput to the output typing in llm,
    async_llm_engine
    - Add validation for max_tokens in EmbeddingRequest
    - Fix __repr__ of EmbeddingSequenceGroupOutput

--- vllm/entrypoints/llm.py
+++ vllm/entrypoints/llm.py
@@ -7,7 +7,7 @@ from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast
 from vllm.engine.arg_utils import EngineArgs
 from vllm.engine.llm_engine import LLMEngine
 from vllm.lora.request import LoRARequest
-from vllm.outputs import RequestOutput
+from vllm.outputs import EmbeddingRequestOutput, RequestOutput
 from vllm.sampling_params import SamplingParams
 from vllm.sequence import MultiModalData
 from vllm.usage.usage_lib import UsageContext
@@ -87,6 +87,7 @@ class LLM:
         enforce_eager: bool = False,
         max_context_len_to_capture: int = 8192,
         disable_custom_all_reduce: bool = True,
+        embedding_mode: bool = False,
         **kwargs,
     ) -> None:
         if "disable_log_stats" not in kwargs:
@@ -107,6 +108,7 @@ class LLM:
             enforce_eager=enforce_eager,
             max_context_len_to_capture=max_context_len_to_capture,
             disable_custom_all_reduce=disable_custom_all_reduce,
+            embedding_mode=embedding_mode,
             **kwargs,
         )
         self.llm_engine = LLMEngine.from_engine_args(
@@ -131,7 +133,7 @@ class LLM:
         use_tqdm: bool = True,
         lora_request: Optional[LoRARequest] = None,
         multi_modal_data: Optional[MultiModalData] = None,
-    ) -> List[RequestOutput]:
+    ) -> List[Union[RequestOutput, EmbeddingRequestOutput]]:
         """Generates the completions for the input prompts.
 
         NOTE: This class automatically batches the given prompts, considering
@@ -205,7 +207,9 @@ class LLM:
                                     lora_request=lora_request,
                                     multi_modal_data=multi_modal_data)
 
-    def _run_engine(self, use_tqdm: bool) -> List[RequestOutput]:
+    def _run_engine(
+            self, use_tqdm: bool
+    ) -> List[Union[RequestOutput, EmbeddingRequestOutput]]:
         # Initialize tqdm.
         if use_tqdm:
             num_requests = self.llm_engine.get_num_unfinished_requests()
@@ -213,7 +217,7 @@ class LLM:
                         desc="Processed prompts",
                         dynamic_ncols=True)
         # Run the engine.
-        outputs: List[RequestOutput] = []
+        outputs: List[Union[RequestOutput, EmbeddingRequestOutput]] = []
         while self.llm_engine.has_unfinished_requests():
             step_outputs = self.llm_engine.step()
             for output in step_outputs:
