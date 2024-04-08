c1eb6e8b6cd93bf53345882c4de6c7d312887d61
Chang Su <chang.s.su@oracle.com>
2024-04-08 11:45:25 -0700

    Add check for EmbeddingRequest input tokens num length

--- vllm/entrypoints/openai/serving_engine.py
+++ vllm/entrypoints/openai/serving_engine.py
@@ -178,7 +178,14 @@ class OpenAIServing:
         token_num = len(input_ids)
 
         # Note: EmbeddingRequest doesn't have max_tokens
-        if isinstance(request, EmbeddingRequest):
+        if (isinstance(request, EmbeddingRequest)
+                and token_num > self.max_model_len):
+            raise ValueError(
+                f"This model's maximum context length is "
+                f"{self.max_model_len} tokens. However, you requested "
+                f"{token_num} tokens in the messages, "
+                f"Please reduce the length of the messages.", )
+        elif isinstance(request, EmbeddingRequest):
             return input_ids
 
         if request.max_tokens is None:
