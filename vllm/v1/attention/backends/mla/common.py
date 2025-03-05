38b5c8aada00616b7e44da624526470152937b45
Lucas Wilkinson <lwilkins@redhat.com>
2025-03-05 02:14:44 +0000

    cleanup
    Signed-off-by: Lucas Wilkinson <lwilkins@redhat.com>

--- vllm/v1/attention/backends/mla/common.py
+++ vllm/v1/attention/backends/mla/common.py
@@ -327,13 +327,34 @@ class MLACommonMetadata(Generic[D]):
     decode: Optional[D] = None
     prefill: Optional[MLACommonPrefillMetadata] = None
 
-    # New for MLA (compared to FlashAttention)
-    # For handling prefill decode split
-    prefill_query_start_loc: Optional[torch.Tensor] = None
-    prefill_max_query_len: Optional[int] = None
-    prefill_block_table: Optional[torch.Tensor] = None
-    decode_seq_lens: Optional[torch.Tensor] = None
-    decode_block_table: Optional[torch.Tensor] = None
+        @dataclass
+        class ChunkedContextMetadata:
+            # New for MLA (compared to FlashAttention)
+            # For handling chunked prefill
+            cu_seq_lens: torch.Tensor
+            starts: torch.Tensor
+            seq_tot: list[int]
+            max_seq_lens: list[int]
+            workspace: torch.Tensor
+
+        # Input positions for rotrary embeddings since for MLA the rotary
+        # position embeddings are applied inside the attention backend
+        input_positions: torch.Tensor
+        block_table: torch.Tensor
+        query_start_loc: torch.Tensor
+        max_query_len: int
+        chunked_context: Optional[ChunkedContextMetadata] = None
+
+    @dataclass
+    class DecodeMetadata:
+        # Input positions for rotrary embeddings since for MLA the rotary
+        # position embeddings are applied inside the attention backend
+        input_positions: torch.Tensor
+        block_table: torch.Tensor
+        seq_lens: torch.Tensor
+
+    decode: Optional[DecodeMetadata] = None
+    prefill: Optional[PrefillMetadata] = None
 
     def __post_init__(self):
         supported_head_sizes = MLACommonBackend.get_supported_head_sizes()
@@ -342,19 +363,6 @@ class MLACommonMetadata(Generic[D]):
             raise ValueError(
                 f"Only {supported_head_sizes} are supported for head_dim,",
                 f"received {self.head_dim}.")
-
-        # Pre-compute prefill/decode tensor slices and other stats
-        if self.num_prefills is not None and self.num_prefills > 0:
-            assert self.num_decodes is not None
-            start = self.num_decodes  # prefill_start
-            self.prefill_query_start_loc = \
-                self.query_start_loc[start:] - self.query_start_loc[start]
-            self.prefill_max_query_len = self.seq_lens[start:].max().item()
-            self.prefill_block_table = self.block_table[start:, ...]
-
-        if self.num_decodes is not None and self.num_decodes > 0:
-            self.decode_seq_lens = self.seq_lens[:self.num_decodes]
-            self.decode_block_table = self.block_table[:self.num_decodes, ...]
 
 
 M = TypeVar("M", bound=MLACommonMetadata)
