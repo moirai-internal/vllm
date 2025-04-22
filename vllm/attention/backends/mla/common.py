762dca9296af3d7fdd29752bb21fc5cf70cd4cb1
Lucas Wilkinson <lwilkinson@neuralmagic.com>
2025-04-22 21:13:16 +0000

    v0 fix
    Signed-off-by: Lucas Wilkinson <lwilkinson@neuralmagic.com>

--- vllm/attention/backends/mla/common.py
+++ vllm/attention/backends/mla/common.py
@@ -880,6 +880,7 @@ class MLACommonMetadataBuilder(AttentionMetadataBuilder[T], Generic[T]):
         if use_captured_graph:
             self.slot_mapping.extend([PAD_SLOT_ID] * cuda_graph_pad_size)
             self.block_tables.extend([] * cuda_graph_pad_size)
+            query_start_loc.extend([query_start_loc[-1]] * cuda_graph_pad_size)
             num_decode_tokens = batch_size - self.num_prefill_tokens
             block_tables = self._get_graph_runner_block_tables(
                 num_seqs, self.block_tables)
